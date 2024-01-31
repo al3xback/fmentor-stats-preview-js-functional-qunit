727a3a9e7c33c493f358f7ddf98f8014201bfb89
alex <al3xback@gmail.com>
2024-01-31 12:50:37 +0700

    added alt and fixed minor

--- js/util.js
+++ js/util.js
@@ -43,7 +43,7 @@ const createCardEl = () => {
 
 	const cardImageEl = createElement('img', null, null, [
 		createElementAttribute('src', './images/meeting.jpg'),
-		createElementAttribute('alt', ''),
+		createElementAttribute('alt', 'People at a meeting'),
 	]);
 
 	cardImageInnerEl.appendChild(cardImageEl);
